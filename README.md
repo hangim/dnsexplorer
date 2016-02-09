f25ae9c1e31848e751ded2226138b423a01dc366
hang <git@hang.im>
2016-02-09 22:28:59 +0800

    dnsexplorer

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dnsexplorer
 Find out available DNS server.
+
+
+First, you should get a DNS server list from below website.
+
+> https://github.com/jedisct1/dnscrypt-proxy/blob/master/dnscrypt-resolvers.csv
+
+Another optional website.
+
+> https://dns.d0wn.biz/
+
+
+You can execute this command to extract ipv4 address to get sorted and unique result.
+
+> grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' dnscrypt-resolvers.csv | sort -nu > server-list.txt
+
+
+Then execute this command to get available DNS server.
+
+> go run dnsexplorer.go < server-list.txt 
+
---
+++ dnsexplorer.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+    "strings"
+    "bufio"
+    "fmt"
+    "os"
+    "os/exec"
+    "sync"
+)
+
+var (
+    server_list chan string
+    port_list []int
+    wg sync.WaitGroup
+)
+
+func check(server string, port int) bool {
+    cmd := exec.Command("dig",
+                        server,
+                        fmt.Sprintf("@%s", server),
+                        fmt.Sprintf("-p%d", port),
+                        "+time=2",
+                        "+tries=3")
+    return cmd.Run() == nil
+}
+
+func main() {
+    server_list = make(chan string)
+    port_list = []int{53, 54, 80, 443, 1053, 5353, 27015} // modify if necessary
+
+    for i := 0; i < 10; i++ {
+        go func() {
+            for {
+                server := <-server_list
+                for _, port := range port_list {
+                    if check(server, port) {
+                        fmt.Printf("%s#%d\n", server, port)
+                        break
+                    }
+                }
+                wg.Done()
+            }
+        }()
+    }
+
+    scanner := bufio.NewReader(os.Stdin)
+    for {
+        line, err := scanner.ReadString('\n')
+        if err != nil {
+            break
+        }
+        server := strings.TrimSpace(line)
+        if len(server) == 0 {
+            continue
+        }
+        wg.Add(1)
+        server_list <- server
+    }
+
+    wg.Wait()
+}
